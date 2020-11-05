0b523786e766d7a2990d5f2f445cf5bed54a5e3e
chrisae9 <chrisae9@gmail.com>
2020-11-04 22:10:41 -0500

    Added creating and updating receipts

--- src/main/resources/static/js/receipt.js
+++ src/main/resources/static/js/receipt.js
@@ -1,38 +1,130 @@
-$("h1").click(function () {
-  console.log("listener");
-});
-
-$("input").keypress(function (event) {
-  if (event.which == 13) {
-    var item = $("input").val();
-    $("#receipt-list ul").append("<li><span> ~ </span>" + item + "</li>");
-    $("input").val("");
-  }
-});
-
-$("#receipt-list ul").on("click", "span", function (event) {
-  $(this)
-    .parent()
-    .fadeOut(200, function () {
-      $(this).remove();
+// Tester function to display the first receipt from the database at the top of the page
+$(document).ready(function() {
+    $.ajax({
+        url: "http://localhost:8080/api/v1/receipt/",
+    }).then(function(data) {
+        $(".receipt-id").append(data[0].id);
+        $(".receipt-name").append(data[0].name);
+        data[0].items.forEach((element) => {
+            $(".receipt-items").append(element.name + " ");
+        });
     });
-  event.stopPropagation();
 });
 
-$.get("navbar.html", function (data) {
-  $("#default-navbar").replaceWith(data);
-});
-
-$(document).ready(function () {
-  $.ajax({
-    url: "http://localhost:8080/api/v1/receipt/",
-  }).then(function (data) {
-    $(".receipt-id").append(data[0].id);
-    $(".receipt-name").append(data[0].name);
-    data[0].items.forEach((element) => {
-      $(".receipt-items").append(element.name + " ");
+// Adds all receipts to the option dropdown menu
+$(document).ready(function() {
+    $.ajax({
+        url: "http://localhost:8080/api/v1/receipt/",
+    }).then(function(data) {
+        data.forEach((element) => {
+            $("select").append(
+                '<option value="' + element.id + '">' + element.name + "</option>"
+            );
+        });
     });
-  });
 });
 
-$;
+$(document).ready(function() {
+    // Action of browser item submit form
+    $("#item-form").submit(function(event) {
+        // this cancels the default action of submitting a form
+        event.preventDefault();
+        var item = $("#item-name").val();
+        var money = parseFloat($("#item-cost").val());
+        addItem(item, money);
+        $("#item-name").val("");
+        $("#item-cost").val("");
+    });
+
+    // Deletes the items from the receipt list
+    $("#receipt-list ul").on("click", "span", function(event) {
+        $(this)
+            .parent()
+            .fadeOut(200, function() {
+                $(this).remove();
+            });
+        event.stopPropagation();
+    });
+
+    $("#save-receipt-button").on("click", function() {
+        alert($("#receipt-select").val());
+        var receipt = {};
+        receipt.name = $("#receipt-name").val();
+        // Creating a receipt does not take an id
+        receipt.items = [];
+        $("#receipt-list li").each(function() {
+            var item = {
+                name: $(this).find("#name").text(),
+                cost: $(this).find("#cost").text(),
+            };
+            receipt.items.push(item);
+        });
+
+        receiptjson = JSON.stringify(receipt);
+
+        console.log(receiptjson);
+
+
+        if ($("#receipt-select").val() != 0) {
+            receipt.id = $("#receipt-select").val();
+            $.ajax({
+                type: "PUT",
+                url: "http://localhost:8080/api/v1/receipt/" + receipt.id,
+                contentType: "application/json",
+                dataType: "json",
+                data: receiptjson,
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(response) {
+                    console.log(response);
+                }
+            });
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/api/v1/receipt/",
+                contentType: "application/json",
+                dataType: "json",
+                data: receiptjson,
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(response) {
+                    console.log(response);
+                }
+            });
+        }
+    });
+
+    // Generic function to add items to the receipt list
+    function addItem(item, cost) {
+        if (typeof cost !== "number" || isNaN(cost)) {
+            cost = 0.0;
+        }
+        $("#receipt-list ul").append(
+            '<li class="list-group-item"><span> <i class="fa fa-trash" aria-hidden="true"></i> </span>' +
+            '<span id="name">' +
+            item +
+            "</span>" +
+            " - $" +
+            '<span id="cost">' +
+            cost.toFixed(2) +
+            "</span>" +
+            "</li>"
+        );
+    }
+
+    // Populates list with selected receipt items
+    $("#receipt-select").on("change", function() {
+        $("#receipt-list ul").empty();
+        $.ajax({
+            url: "http://localhost:8080/api/v1/receipt/" + $(this).val(),
+        }).then(function(data) {
+            $("#receipt-name").val(data.name);
+            data.items.forEach((element) => {
+                addItem(element.name, element.cost);
+            });
+        });
+    });
+});
