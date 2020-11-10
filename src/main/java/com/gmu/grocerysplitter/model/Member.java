6d4998c40f387eb6dc93c3ac8d1e353388053944
fahersi <f.hersi.96@gmail.com>
2020-11-10 12:54:41 -0500

    Member HomePage - V3

---
+++ src/main/java/com/gmu/grocerysplitter/model/Member.java
@@ -0,0 +1,46 @@
+package com.gmu.grocerysplitter.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Member {
+
+    private final UUID id;
+
+    @NotBlank
+    private final String userEmail;
+    @NotBlank
+    private final String fName;
+    @NotBlank
+    private final String lName;
+
+    private final List<Receipt> receipts;
+    private final List<Bill> bills;
+
+    public Member(@JsonProperty("id") UUID id, @JsonProperty("userEmail") String userEmail, @JsonProperty("fName") String fName, 
+            @JsonProperty("lName") String lName, @JsonProperty("receipts") List<Receipt> receipts, @JsonProperty("bills") List<Bill> bills)
+    {
+        this.id = id;
+        this.userEmail = userEmail;
+        this.fName = fName;
+        this.lName = lName;
+        this.receipts = receipts;
+        this.bills = bills;
+    }
+    
+    public UUID getID () {return this.id;}
+
+    public String getFirstname() {return this.fName;}
+    public String getLastname() {return this.lName;}
+    public String getUserEmail() {return this.userEmail;}
+
+    public List<Receipt> getReciepts() {return this.receipts;}
+    public List<Bill> getBills() {return this.bills;}
+    
+    
+}
