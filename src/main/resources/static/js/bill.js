397542c0111840843232bc02f40c5a43ef65203f
mnousain <nousain24@gmail.com>
2020-12-08 11:10:19 -0500

    Updated billManagement.js

--- src/main/resources/static/js/bill.js
+++ src/main/resources/static/js/bill.js
@@ -105,15 +105,28 @@ $(document).ready(function () {
         '</tr>'
     );
     
+    var testID = item + "-contributors";
+    //alert("TEST: " + testID)
+    var testID2;
+    document.getElementById("contributors").id = testID;
+    for(var i = 0; i < contributors.length; i++)
+    	{
+    	testID2 = item + "-" + contributors[i];
+        document.getElementById(testID).innerHTML +=
+        	'<div class="form-check">' +
+      		'<input class="form-check-input" type="checkbox" id=' + testID2 + ' value="option1">' +
+         	'<label class="form-check-label" for=' + testID2 + '>' + 
+         	contributors[i] + 
+         	'</label>' +
+         	'</div>'
+    	}
+
  
     var newTotal = parseFloat($("#total-cost").text()) + cost;
     $("#total-cost").text(newTotal.toFixed(2));
     
   }
-  
-});
-
-$("#calculate-bill").on("click", function (event) {
+  $("#calculate-bill").on("click", function (event) {
 	   event.preventDefault();
 	   var billName = $("#bill-name").val();
 	   var billDate = $("#bill-date").val();
@@ -122,6 +135,76 @@ $("#calculate-bill").on("click", function (event) {
 	   $("#bill-name").focus();
 	   $("#bill-date").val("");
 	   $("#bill-date").focus();
+	   getTableData();
+	   
+	   
+	   function getTableData(){
+		   var itemContributors  =[];
+		   var contribBillID;
+		   
+		   //initialize a table for each contributor
+		   for(var i = 0; i < contributors.length; i++)
+			   {
+			   		contribBillID = contributors[i] + "-billTable";
+			   		alert("Table Created For: " + contributors[i] + "ID: " + contribBillID);
+			   		
+			   		document.getElementById("bill").innerHTML +=
+			   			'<h5>' + contributors[i] +'</h5>' +
+			   			'<div class="d-flex justify-content-center p-2">' +
+			   				'<ul class="items list-group"></ul>' +
+			   					'<table id=' + contribBillID + ' class="table table-bordered">' +
+			   						'<thead>' +
+			   							'<tr>' +       
+					                        '<th scope="col" style="text-align:center">Item</th>' +
+					                        '<th scope="col" style="text-align:center">Cost</th>' +
+					                    '</tr>' +
+					                '</thead>'
+					                '<tbody>' +
+					                '</tbody>' +
+					            '</table>' +
+					    '</div>'
+				}
+		   
+		   $("#receipt-table tbody tr").each(function(){
+			   var id;
+			      var item = {
+			    	        name: $(this).find("#name").text(),
+			    	        cost: $(this).find("#cost").text(),
+			    	      };
+			      
+			      var checkboxName;
+			      for(var i = 0; i < contributors.length; i++)
+			    	  {			    	  
+			    	  checkboxName = item.name + "-" + contributors[i];
+			    	  var checkbox = document.getElementById(checkboxName).checked;
+			    	  if(checkbox)
+			    		  {
+			    		  //alert("Contributor: '" + contributors[i] + "' added to contributor list for item: '" + item.name + "'");
+			    		  itemContributors.push(contributors[i]);
+			    		  }
+			    	  }
+			      //alert("Item: " + item.name + ", Cost: " + item.cost + ", # of Contributors: " + itemContributors.length)
+			      item.cost = item.cost / itemContributors.length;
+			      //alert("NEW COST -> Item: " + item.name + ", Cost: " + item.cost)
+			      
+			      //add item and cost to contributor table
+			      for(var i = 0; i < itemContributors.length; i++)
+			    	  {
+			    	  	id = itemContributors[i] + "-billTable";
+			    	  	document.getElementById(id).innerHTML += 
+			    	  		'<tr>' +
+			    	  			'<td id="name">' +
+			    	  			item.name +
+			    	  			'</td>' +
+			    	  			'<td id="cost">$' +
+			    	  			item.cost.toFixed(2) +
+			    	  			'</td>' +
+			    	  		'</tr>'	    	  			
+			    	  }
+			      
+			   itemContributors = []; //reset item contributors
+		   })
+	   }
 	   
 	   function addBillNameDate(billName, billDate){
 	  	   $("#bill").append(
@@ -132,6 +215,7 @@ $("#calculate-bill").on("click", function (event) {
 	  	  		billDate +
 	  	  		'</h4>' +
 	  	  		'<hr>'
-	  	   );  	  	  	   
+	  	   );  	  
 	   }
 	});
+});
